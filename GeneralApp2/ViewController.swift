6b46b6d5c518402850443e109868ab830c5b4c64
Den Andreychuk <denis.andrei4uk@gmail.com>
2023-11-22 22:43:34 +0200

    Init commit

--- GeneralApp2/ViewController.swift
+++ GeneralApp2/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  GeneralApp2
-//
-//  Created by Den Andreychuk on 22.11.2023.
-//
-
 import UIKit
+import AVFoundation
+import Lottie
 
 class ViewController: UIViewController {
+    
+    private let player = AVQueuePlayer()
+    
+    private var timer: Timer?
+    
+    private lazy var animationView = {
+        let view = LottieAnimationView()
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        try! AVAudioSession.sharedInstance().setCategory(.playback)
+        try! AVAudioSession.sharedInstance().setActive(true)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { _ in
+            print("Running...")
+        })
+        
+        player.replaceCurrentItem(with: AVPlayerItem(url: Bundle.main.url(forResource: "sample", withExtension: "mp3")!))
+        player.play()
+        
+        view.addSubview(animationView)
+        animationView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        animationView.animation = .named("test")
+        playAnimation()
     }
-
-
+    
+    private func playAnimation() {
+        self.animationView.play { completed in
+            print("Animation completed: \(completed)")
+            self.playAnimation()
+        }
+    }
 }
-
---
+++ Pods/Target Support Files/Pods-GeneralApp2/Pods-GeneralApp2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GeneralApp2 : NSObject
+@end
+@implementation PodsDummy_Pods_GeneralApp2
+@end
