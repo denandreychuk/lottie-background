075f9be3d57f33977e4a7c55bbe5752d572d4885
Den Andreychuk <denis.andrei4uk@gmail.com>
2023-11-22 22:10:42 +0200

    Initial Commit

---
+++ GeneralApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeneralApp2
+//
+//  Created by Den Andreychuk on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
